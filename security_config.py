901ae2d030c580ac215baecfe01a960db514e7b8
aisecurefuture <p@gratitech.com>
2025-01-26 15:37:08 -0800

    Create security_config.py

---
+++ security_config.py
@@ -0,0 +1,42 @@
+SECRET_KEY = ''
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///yourDB.db'
+
+GOOGLE_CLIENT_ID = ''
+
+GOOGLE_CLIENT_SECRET = ''
+
+EMAIL_FROM = ''
+
+SMTP_SERVER = ''
+
+SMTP_PORT = ''
+
+SMTP_PASSWORD = ''
+
+SMTP_USER = ''
+
+LOG_PATH = ''
+
+STR_APP_KEY = ''
+
+PROMPT_INJECTION_JAIL_MODEL_PATH = ''
+
+PROMPT_INJECTION_JAIL_VECTORIZER_PATH = ''
+
+OPEN_AI_K = ''
+
+ANTHROPIC_K = ''
+
+GOOGLE_K = ''
+
+PERPLEXITY_K = ''
+STRIPE_K = ''
+
+STRIPE_SignK = ''
+
+GOOGLE_Recaptcha_Key_ID = ''
+
+GOOGLE_Recaptcha_Project_ID = ''
+
+GOOGLE_Recaptcha_Secret_Key = ''
